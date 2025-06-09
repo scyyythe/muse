dcfd6ffd8c69cfc7b4811ccc9aa813a00f24e734
scyyythe <caneteangel187@gmail.com>
2025-06-09 23:51:35 +0800

    added social icons

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@react-navigation/elements";
 import React, { useState } from "react";
 import { Image, StyleSheet, Text, View } from "react-native";
 import Swiper from "react-native-web-swiper";
@@ -23,11 +24,38 @@ export default function WelcomeScreen() {
           <Text style={styles.title}>Ready to explore?</Text>
           <Text style={styles.description}>Swipe through and start discovering your favorite tunes.</Text>
         </View>
+
+        <View style={styles.page}>
+          <Text style={styles.title}>Join Us</Text>
+          <Text style={styles.description}>Log in or register now to start your musical journey with Muse.</Text>
+
+          <View style={styles.buttonContainer}>
+            <Button variant="filled" color="black">
+              Create an account
+            </Button>
+            <View style={styles.IconsContainer}>
+              <Text style={styles.signWith}> or sign in with </Text>
+              <View style={styles.socialContainer}>
+                <Image source={require("@/assets/icons/gmail.png")} style={styles.socialIcon} />
+                <Image source={require("@/assets/icons/facebook.png")} style={styles.socialIcon} />
+                <Image source={require("@/assets/icons/apple.png")} style={styles.socialIcon} />
+              </View>
+            </View>
+
+            <Text style={styles.signInPrompt}>
+              Already have an account?{" "}
+              <Text style={styles.signInLink} onPress={() => console.log("Navigate to login")}>
+                Sign in
+              </Text>
+            </Text>
+          </View>
+        </View>
       </Swiper>
 
       <View style={styles.indicatorWrapper}>
-        <View style={[styles.indicatorLine, index === 0 ? styles.active : styles.inactive]} />
-        <View style={[styles.indicatorLine, index === 1 ? styles.active : styles.inactive]} />
+        {[0, 1, 2].map((i) => (
+          <View key={i} style={[styles.indicatorLine, index === i ? styles.active : styles.inactive]} />
+        ))}
       </View>
     </View>
   );
@@ -50,7 +78,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "center",
     marginBottom: 70,
-    gap: 10,
+    gap: 6,
   },
   textContainer: {
     alignSelf: "flex-start",
@@ -60,19 +88,26 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 26,
-    fontWeight: "bold",
+    fontFamily: "Poppins_700Bold",
     marginBottom: 10,
     textAlign: "left",
     alignSelf: "flex-start",
   },
   description: {
     fontSize: 16,
+    fontFamily: "Poppins_400Regular",
     textAlign: "left",
     lineHeight: 25,
     alignSelf: "flex-start",
   },
+  signWith: {
+    fontSize: 13,
+    fontFamily: "Poppins_400Regular",
+    color: "gray",
+    textAlign: "center",
+  },
   indicatorLine: {
-    width: 10,
+    width: 8,
     height: 4,
     borderRadius: 2,
     transitionProperty: "width, background-color",
@@ -86,5 +121,48 @@ const styles = StyleSheet.create({
 
   inactive: {
     backgroundColor: "#ccc",
+  },
+  registerButton: {
+    marginTop: 20,
+    width: "100%",
+    backgroundColor: "black",
+    borderRadius: 6,
+  },
+
+  buttonContainer: {
+    flexDirection: "column",
+    justifyContent: "space-between",
+    width: "100%",
+    marginTop: 30,
+    gap: 10,
+    fontFamily: "Poppins_700Bold",
+  },
+  signInPrompt: {
+    fontSize: 12,
+    fontFamily: "Poppins_400Regular",
+    textAlign: "center",
+    color: "#555",
+    marginTop: 10,
+  },
+
+  signInLink: {
+    fontSize: 12,
+    fontFamily: "Poppins_400Regular",
+    color: "#007AFF",
+  },
+  socialContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    gap: 20,
+    marginTop: 20,
+    marginBottom: 15,
+  },
+  socialIcon: {
+    width: 30,
+    height: 30,
+    borderRadius: 20,
+  },
+  IconsContainer: {
+    marginTop: 20,
   },
 });
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,4 @@
+import { Poppins_400Regular, Poppins_700Bold } from "@expo-google-fonts/poppins";
 import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
@@ -8,8 +9,10 @@ import { useColorScheme } from "@/hooks/useColorScheme";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+
   const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    Poppins_400Regular,
+    Poppins_700Bold,
   });
 
   if (!loaded) {
